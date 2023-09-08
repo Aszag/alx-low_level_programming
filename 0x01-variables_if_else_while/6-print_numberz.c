7788afdc1fbc6619010ab4701acc77a4fc981df3
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-08 15:00:10 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Print numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
