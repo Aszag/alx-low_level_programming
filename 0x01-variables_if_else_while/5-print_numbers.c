2665b25cf57419d84723f0b6b20f9fc614389eaf
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-08 14:13:30 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Print numbers
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int = n;
+		while (n <= 9)
+		{
+			printf("%d", n);
+			n++;
+		}
+	printf("\n");
+	return (0);
+}
