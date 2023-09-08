52205eca782bde2dca7ddb63e04f7f2779703540
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-08 05:56:17 -0700

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase and uppercase
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha = 'a';
+	char ALPHA = 'A';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++
+	}
+	while (ALPHA <= 'Z')
+	{
+		putchar(ALPHA);
+		ALPHA++
+	}
+	putchar('\n');
+	return (0);
+
+}
