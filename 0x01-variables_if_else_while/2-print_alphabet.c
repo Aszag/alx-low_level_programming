6946c40afe77ad3965c836548c540a433049e112
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-08 05:27:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char = alpha
+	while (char <= z)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
