573debbbb0b97995e0885f28d207588dd8f09dbb
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-12 11:49:57 -0700

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 1 ; i <= 10 ; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar("\n");
 	}
