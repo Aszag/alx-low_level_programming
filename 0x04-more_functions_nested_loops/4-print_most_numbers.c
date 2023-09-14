7e60e20c384ccd56eb04b6f5013bbaa137005a1b
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 14:37:52 -0700

    loup

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers -to print numbers beside 2 and 4
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		else
+			_putchar(n + '0');
+	}
+}
