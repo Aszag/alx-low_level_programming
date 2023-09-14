8fa5d388b8e0451a02db6bae5487633808c06306
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 15:29:13 -0700

    loup

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ * print_line -To print lines
+ * Retunr: 0
+ * @n: is the number of lines
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+}
