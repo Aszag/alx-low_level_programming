49594dbda4ea25854937cd82cb71e7c23c2f3f73
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 15:05:20 -0700

    loup

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-	for (j = 0; j <= 14; j++)
-	{
-		if (j < 9)
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
 		{
 			_putchar((j / 10) + '0');
 		}
