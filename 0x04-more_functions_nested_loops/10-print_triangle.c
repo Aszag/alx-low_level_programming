ed269d17a8326d5565a73ee27dead31a228ba9f7
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 16:49:38 -0700

    loup

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle -to print a triangle
+ * @size: the size of triangle
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if ((x + y) < size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
