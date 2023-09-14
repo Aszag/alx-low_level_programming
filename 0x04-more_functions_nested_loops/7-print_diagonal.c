c1a36d2745345630455087f1d65829baa265dc1f
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 16:13:46 -0700

    loup

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,9 +9,7 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (x = 1; x <= n; x++)
@@ -20,8 +18,8 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
-		_putchar(92);
-		_putchar('\n');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }
