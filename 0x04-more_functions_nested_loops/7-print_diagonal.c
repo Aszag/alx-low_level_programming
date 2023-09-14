4989cae100b41cf2bb11f1f6b02b47ea15802d28
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-14 15:51:57 -0700

    loup

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal -to print diagonal line
+ * @n: is the number of times
+ * Return:0
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+			for (y = 1; y <= x; y++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+	}
+}
