4228b6652ba43737fba68e752b39e9c015bf9ffd
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-09-28 16:39:57 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,16 +19,13 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int m)
 {
-	if (m >= n && n >= 2)
+	if (n <= 1 || (n != m && n % m == 0))
+	{
+		return (0);
+	}
+	else if (n == m)
 	{
 		return (1);
 	}
-	else if (n % m == 0 || n <= 1)
-	{
-	       return (0);
-	}
-	else
-	{
-		return (check_prime(n, m + 1));
-	}
+	return (check_prime(n, m + 1));
 }
