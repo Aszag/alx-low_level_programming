3d0e954b9d8e7d5737d9212fd4de2c036066c32e
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-02 13:35:00 -0700

    static

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - a fonction to print a string
+ * @dest: a poniter
+ * @src: a pointer also
+ * @n: a byte
+ * Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i];
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
+	return (dst);
+}
