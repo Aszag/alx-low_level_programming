1e250495ac6475617aa3d7e5bd6958897246589c
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-04 14:23:39 -0700

    numbers

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -to print all arguments
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
