a510f0c216920f2ddde87a3688f3764913ac41ca
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-04 15:18:55 -0700

    sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include<stdio>
+#include<sdtlib>
+/**
+ * main -print sum of numbers
+ * @argc: the number of argument
+ * @argv: array of pointer
+ * Return: 0, or 1 if symbols are not digit
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
