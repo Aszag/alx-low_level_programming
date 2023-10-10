2f786d048898fa52dec18f06da9610ee7ebd08cb
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-10 11:20:06 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owne: owner's name
+ * Description: dog's information and his owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
