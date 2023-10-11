d87abfffac87c508edc7a35ea58016cc72c7798c
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-11 08:42:25 -0700

    dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include<stdio.h>
+/**
+ * new_dog - to creat a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+
+}
