1e81f26d404e551de3f5bab6d6d3c3d45feefac3
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-11 08:47:46 -0700

    dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * free_dog - free dogs
+ * @d: dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			free(d->name)
+		if (d->owner)
+			free(d->owner)
+		free(d);
+	}
+}
