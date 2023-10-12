3b5aef7b80df27ad586c68decc1e9b4962af1bd2
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-12 13:56:26 -0700

    variadic

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+/**
+ * print_all - to print all
+ * @format: list of types of arguments
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *str, *s = "";
+	va_list ls;
+
+	va_start(ls, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", s, va_arg(ls, int));
+					break;
+				case 'i':
+					printf("%s%d", s, va_arg(ls, int));
+					break;
+				case 'f':
+					printf("%s%f", s, va_arg(ls, double));
+					break;
+				case 's':
+					printf("%s%s", s, va_arg(ls, char *));
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", s, str);
+					break;
+					default;
+					i++;
+					continue;
+			}
+			s = ", ";
+			i++;
+		}
+	}
+}
