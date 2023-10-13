2d50c21d52a98d65d8b7b0492e7a41a7f476217b
asmaa zagraoui <asmaazagraoui@gmail.com>
2023-10-13 13:09:17 -0700

    fonction

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - to print aname
+ * @name: the name 
+ * @f: a pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
